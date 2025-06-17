678fb5d29e1701bac36f221b8123b7cce8c4442f
marwabounaas2 <marwabounaas05@gmail.com>
2025-06-17 10:20:04 +0200

    versoin11

---
+++ build/web/calcul.jsp
@@ -0,0 +1,21 @@
+<%-- 
+    Document   : calcul
+    Created on : 17 juin 2025, 09:23:06
+    Author     : marwa
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>la moyenne est ${M}</h1>
+        
+        
+        
+        
+    </body>
+</html>
---
+++ src/java/model/etudaint.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author marwa
+ */
+public class etudaint {
+    String nom;
+    int note1;
+    int note2;
+
+    public etudaint(String nom, int note1, int note2) {
+        this.nom = nom;
+        this.note1 = note1;
+        this.note2 = note2;
+    }
+    public float calcul(int note1, int note2)
+    {
+        return(note1+note2)/2;
+    }
+}
